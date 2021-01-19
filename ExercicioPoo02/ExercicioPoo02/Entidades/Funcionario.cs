7c216f5c47fa01a5eb79e3632fa5a0301bcc9e32
Dolglas Mesquita <dolglas_mesquita@hotmail.com>
2021-01-19 00:29:03 -0300

    Exercícios POO

---
+++ ExercicioPoo02/ExercicioPoo02/Entidades/Funcionario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPoo02.Entidades
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+    }
+}
